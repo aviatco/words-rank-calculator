a0f808126dedc870f45db6a248b82842b01fbfa5
aviatco <aviatco@gmail.com>
2021-02-06 19:35:25 +0200

    remove async

--- Backend/src/handlers/words-rank-calculator.handler.ts
+++ Backend/src/handlers/words-rank-calculator.handler.ts
@@ -4,7 +4,7 @@ import { WordsRankResultDto } from "../dtos/words-rank-result.model";
 import { WordRankHelper } from "../utils/words-rank.helper";
 
 
-export const wordsRateCalculator = async (request: Request, response: Response, next: NextFunction): Promise<void>  => {
+export const wordsRateCalculator = (request: Request, response: Response, next: NextFunction): void  => {
     try{
         const content: string[] = WordRankHelper.extractCleanContent(response.locals.topicContent);
         if(content.length === 0) next(new HttpException(204, 'No content'));
